77241eb2313dca160d980f82f7cf02c012c77b80
LeeSuHyeong <lsg1454@naver.com>
2019-03-22 01:56:59 +0900

    https://code.sasa.hs.kr/ JAVA Practice 1001 ~ 1010

---
+++ src/prac_1009.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class prac_1009 {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		byte abc = s.nextByte();
+		abc++;
+		System.out.println(abc);
+		
+		//System.out.println(abc+1); 라고 했더니 오답 처리 당했다 왜 그런지는 이유를 알 수 X
+	}
+}
